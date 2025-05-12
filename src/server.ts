b27c06f170b55ecf9331a3693ad0f20f87848420
BrustS <s3rg3yburak@ya.ru>
2025-05-13 01:18:25 +0300

    feat: cluster feature

--- src/server.ts
+++ src/server.ts
@@ -21,53 +21,51 @@ const requestListener = function (
   const path = url.pathname;
   const method = req.method;
 
-
   res.setHeader("Content-Type", "application/json");
 
   if (path === "/api/users" && method === "GET") {
     res.statusCode = 200;
-    res.end(JSON.stringify({ result: getUsers()}));
+    res.end(JSON.stringify({ result: getUsers() }));
   } else if (path === "/api/users" && method === "POST") {
     let body = "";
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
     req.on("end", () => {
-        console.log(body);
       const name = JSON.parse(body);
       const result = createUser(name);
       res.statusCode = result.status;
       result.status === 201
         ? res.end(JSON.stringify(result.data))
-        : res.end(JSON.stringify(result.message));
+        : res.end(JSON.stringify({ error: result.message }));
     });
   } else if (path.startsWith("/api/users/")) {
     const id = path.split("/").pop() || "";
     if (method === "GET") {
       const findUser = getUser(id);
-      res.writeHead(findUser.status);
+      res.statusCode = findUser.status;
       findUser.status === 200
         ? res.end(JSON.stringify(findUser.data))
-        : res.end(JSON.stringify(findUser.message));
+        : res.end(JSON.stringify({ error: findUser.message }));
     } else if (method === "DELETE") {
       const deleted = deleteUser(id);
-      res.writeHead(deleted.status);
+      res.statusCode = deleted.status;
       deleted.message
         ? res.end(JSON.stringify({ message: deleted.message }))
         : res.end();
     } else if (method === "PUT") {
-        let body = "";
-        req.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-        req.on("end", () => {
-          const name = JSON.parse(body);
-          const result = updateUser(id,name);
-          res.writeHead(result.status);
-          result.status === 200
-            ? res.end(JSON.stringify(result.data))
-            : res.end(JSON.stringify(result.message));
-        });
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        const name = JSON.parse(body);
+        const result = updateUser(id, name);
+        res.statusCode = result.status;
+        result.status === 200
+          ? res.end(JSON.stringify(result.data))
+          : res.end(JSON.stringify({ error: result.message }));
+      });
     }
   }
 };
