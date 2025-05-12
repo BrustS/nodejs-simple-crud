06b7fd3a05c47bb6d87c2dcb52b2dee1d420d803
BrustS <s3rg3yburak@ya.ru>
2025-05-13 01:03:14 +0300

    feat: crud api release

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,81 @@
-import * as http from 'http';
-import * as dotenv from 'dotenv';
-import process from 'process';
+import * as http from "http";
+import * as dotenv from "dotenv";
+import process from "process";
+import {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "./controllers/users.controller";
 
 dotenv.config();
 
-const port = parseInt(process.env.PORT || '4000', 10);
+const port = parseInt(process.env.PORT || "4000", 10);
 
-const requestListener = function (req: http.IncomingMessage, res: http.ServerResponse) {
-    const url = new URL(req.url || '', `http://localhost:${port}`);
-    const path = url.pathname;
-    const method = req.method;
+const requestListener = function (
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+) {
+  const url = new URL(req.url || "", `http://localhost:${port}`);
+  const path = url.pathname;
+  const method = req.method;
 
-    res.setHeader('Content-Type', 'application/json');
+
+  res.setHeader("Content-Type", "application/json");
+
+  if (path === "/api/users" && method === "GET") {
+    res.statusCode = 200;
+    res.end(JSON.stringify({ result: getUsers()}));
+  } else if (path === "/api/users" && method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+        console.log(body);
+      const name = JSON.parse(body);
+      const result = createUser(name);
+      res.statusCode = result.status;
+      result.status === 201
+        ? res.end(JSON.stringify(result.data))
+        : res.end(JSON.stringify(result.message));
+    });
+  } else if (path.startsWith("/api/users/")) {
+    const id = path.split("/").pop() || "";
+    if (method === "GET") {
+      const findUser = getUser(id);
+      res.writeHead(findUser.status);
+      findUser.status === 200
+        ? res.end(JSON.stringify(findUser.data))
+        : res.end(JSON.stringify(findUser.message));
+    } else if (method === "DELETE") {
+      const deleted = deleteUser(id);
+      res.writeHead(deleted.status);
+      deleted.message
+        ? res.end(JSON.stringify({ message: deleted.message }))
+        : res.end();
+    } else if (method === "PUT") {
+        let body = "";
+        req.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+        req.on("end", () => {
+          const name = JSON.parse(body);
+          const result = updateUser(id,name);
+          res.writeHead(result.status);
+          result.status === 200
+            ? res.end(JSON.stringify(result.data))
+            : res.end(JSON.stringify(result.message));
+        });
+    }
+  }
 };
 
 const server = http.createServer(requestListener);
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port} with process id ${process.pid}`);
+  console.log(
+    `Server listening on port ${port} with process id ${process.pid}`
+  );
 });
